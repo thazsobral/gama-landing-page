6d9b9ec53839396d38bc79a6a7d8c2cbcd4f846e
thazsobral <thazsobral@gmail.com>
2021-07-17 21:46:35 -0300

    add subscribe-news

--- scripts/script.js
+++ scripts/script.js
@@ -4,11 +4,12 @@ form.addEventListener("submit", (event) => {
     event.preventDefault();
     let name = document.querySelector("#name").value;
     let email = document.querySelector("#email").value;
+    let news = document.querySelector("#subscribe-news").value;
 
-    localStorage.setItem("dataUser",JSON.stringify({"name": name, "email": email}));
+    localStorage.setItem("dataUser", JSON.stringify({ "name": name, "email": email, "subscribe-news": news }));
 
     setTimeout(() => {
         form.innerHTML = "<p>Prontinho!<br />Agora é verificar nosso e-mail de confirmação na sua caixa de entrada.</p>";
     }, 1500);
-    
+
 });
