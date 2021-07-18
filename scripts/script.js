7f414d05157bc94818e407d2855ba2c9b2b59006
thazsobral <thazsobral@gmail.com>
2021-07-18 03:29:24 -0300

    add verification in form

--- scripts/script.js
+++ scripts/script.js
@@ -4,13 +4,16 @@ form.addEventListener("submit", (event) => {
     event.preventDefault();
     let name = document.querySelector("#name").value;
     let email = document.querySelector("#email").value;
-    let news = document.querySelector("#subscribe-news").value;
+    let news = document.querySelector("#subscribe-news").checked;
 
-    localStorage.setItem("dataUser", JSON.stringify({ "name": name, "email": email, "subscribe-news": news }));
+    if (name == "" || email == "") alert("Antes de se inscrever é necessário preencher nome e e-mail.")
+    else {
+        localStorage.setItem("dataUser", JSON.stringify({ "name": name, "email": email, "subscribe-news": news }));
 
-    setTimeout(() => {
-        let formContainer = document.querySelector(".form-container");
-        formContainer.innerHTML = "<p>Prontinho!<br />Agora é verificar nosso e-mail de confirmação na sua caixa de entrada.</p>";
-    }, 1500);
+        setTimeout(() => {
+            let formContainer = document.querySelector(".form-container");
+            formContainer.innerHTML = "<p>Prontinho!<br />Agora é verificar nosso e-mail de confirmação na sua caixa de entrada.</p>";
+        }, 1500);
+    }
 
 });
